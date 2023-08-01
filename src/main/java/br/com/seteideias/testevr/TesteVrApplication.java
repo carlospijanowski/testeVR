f69ac07d6d896624487c0995d2a65f0c506b3d4b
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2023-08-01 00:04:48 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/testevr/TesteVrApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.testevr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteVrApplication.class, args);
+    }
+
+}
